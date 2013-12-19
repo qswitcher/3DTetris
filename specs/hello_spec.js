9e3bfcab7c0dd8c3acb4dad1bc508a1bc0072aa9
Jeffrey Russom <jrussom@homeaway.com>
2013-12-18 23:53:33 -0600

    Setup gruntfile

---
+++ specs/hello_spec.js
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: jrussom
+ * Date: 12/18/13
+ * Time: 11:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+describe("A suite", function() {
+    it("contains spec with an expectation", function() {
+        expect(true).toBe(true);
+    });
+});
